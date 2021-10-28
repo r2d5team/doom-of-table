4264f446d9b2b4e744fb92d3d815750393232add
thisisariann <ari.menendez13@gmail.com>
2021-10-28 11:16:59 +0200

    js func randomMembers

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const members = ["Lucas", "Biplop", "Sergio", "Ariann"];
+
+/*for (const object of members) {
+  console.log(object);
+}*/
+
+/*IMPRIMIR EN HTML LOS MEMBERS */
+
+/*Q SELECCIONE ALEATORIAMENTE*/
+
+/*RECORRER FUNCIÓN ALEATORIAMENTE*/
+function randomMembers() {
+  const index = Math.floor(Math.random() * members.length);
+  console.log(members[index]);
+}
+
+randomMembers();
+
+/*<section class="button1">
+          <img
+            class="iconsButton"
+            src="/css/img/icons/kill.png"
+            ;
+            alt="Hand with  knife image"
+          />
+          <p class="memberTitle">Member 1</p>
+          <img
+            class="iconsButton"
+            src="/css/img/icons/trash.svg"
+            ;
+            alt="Trash can image"
+          />
+        </section>*/
